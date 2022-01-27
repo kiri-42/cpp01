f55120ce8c0453f097ab60bfed63317a4eeea4f8
kiri0314 <t.kirihara0314@gmail.com>
2022-01-27 16:25:45 +0900

    add: ex01

---
+++ ex01/Zombie.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/27 15:21:02 by tkirihar          #+#    #+#             */
+/*   Updated: 2022/01/27 16:04:36 by tkirihar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string zombie_name)
+{
+	name = zombie_name;
+}
+
+Zombie::~Zombie()
+{
+	std::cout << "Deleted " << name << std::endl;
+}
+
+void	Zombie::announce(void)
+{
+	std::cout << name << " BraiiiiinnnzzzZ..." << std::endl;
+}
